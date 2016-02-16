e6ba710216063bc2a24a511d714a323b06102ada
Christopher Erdos <chris.erdos@gmail.com>
2016-02-16 12:24:10 -0600

    added assign uid view and vc

---
+++ Chortal/AssignUIDViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AssignUIDViewController.swift
+//  Chortal
+//
+//  Created by Christopher Erdos on 2/16/16.
+//  Copyright © 2016 JonerDos. All rights reserved.
+//
+
+import UIKit
+
+class AssignUIDViewController: UIViewController {
+    //MARK: Properties
+    
+    
+    //MARK: Outlets
+    
+    @IBOutlet weak var uidTextField: UITextField!
+    
+    
+    //MARK: View Loading
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK: Custom Functions
+    
+    //MARK: IBActions
+    
+    //MARK: Delegate Functions
+    
+    //MARK: Segue
+
+}
