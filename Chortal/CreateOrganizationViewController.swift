8ecd8836f4aa77ff9da5c8f47bef7813e3e6957c
Christopher Erdos <chris.erdos@gmail.com>
2016-02-16 10:28:21 -0600

    set up create organization vc

--- Chortal/CreateOrganizationViewController.swift
+++ Chortal/CreateOrganizationViewController.swift
@@ -8,12 +8,18 @@
 
 import UIKit
 
-class CreateOrganizationViewController: UIViewController {
+class CreateOrganizationViewController: UIViewController, UITextFieldDelegate {
     //MARK: Properties
     
     
     //MARK: Outlets
     
+    @IBOutlet weak var organizationNameTextField: UITextField!
+    @IBOutlet weak var adminNameTextField: UITextField!
+    @IBOutlet weak var adminPasswordTextField: UITextField!
+    @IBOutlet weak var organizationTypeTextField: UITextField!
+    @IBOutlet var memberNameTextFields: [UITextField]!
+    @IBOutlet var memberPasswordTextFields: [UITextField]!
     
     //MARK: View Loading
     override func viewDidLoad() {
@@ -24,6 +30,8 @@ class CreateOrganizationViewController: UIViewController {
     
     //MARK: IBActions
     
+    @IBAction func createOrganizationTap(sender: UIButton) {
+    }
     //MARK: Delegate Functions
     
     //MARK: Segues
