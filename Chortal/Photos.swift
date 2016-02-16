37a240dd6d88f8bd6075e197f5091ee277a762af
Kanybek Zhagusaev <zhagusaev@gmail.com>
2016-02-16 17:17:32 -0600

    added new custom class for camera

---
+++ Chortal/Photos.swift
@@ -0,0 +1,13 @@
+//
+//  Photos.swift
+//  Chortal
+//
+//  Created by Kanybek Zhagusaev on 2/16/16.
+//  Copyright © 2016 JonerDos. All rights reserved.
+//
+
+class Photos {
+    
+    
+    
+}
--- Chortal/TaskViewController.swift
+++ Chortal/TaskViewController.swift
@@ -8,21 +8,24 @@
 
 import UIKit
 
-class TaskViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class TaskViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     //MARK: Properties
     
     
     //MARK: Outlets
     
+    @IBOutlet weak var collectionViewFlow: UICollectionViewFlowLayout!
     @IBOutlet weak var taskNameLabel: UILabel!
     @IBOutlet weak var timerLabel: UILabel!
     @IBOutlet weak var descriptionTextView: UITextView!
     @IBOutlet weak var collectionView: UICollectionView!
-    
+    var images = [UIImage]()
     
     //MARK: View Loading
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        collectionViewFlow.itemSize = CGSizeMake(collectionView.frame.width/3, collectionView.frame.width/3)
     }
     
     //MARK: Custom Functions
@@ -31,17 +34,39 @@ class TaskViewController: UIViewController, UICollectionViewDataSource, UICollec
     
     //MARK: Delegate Functions
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
+        return self.images.count
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("pizza", forIndexPath: indexPath)
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("pizza", forIndexPath: indexPath) as! CustomCamCollectionViewCell
+        cell.imageView.image = images[indexPath.item]
         
         return cell
     }
     
-    
+    @IBAction func onCameraButtonTapped(sender: AnyObject) {
+        
+            let picker = UIImagePickerController()
+            picker.delegate = self
+            picker.allowsEditing = true;
+            picker.sourceType = UIImagePickerControllerSourceType.Camera
+            self.presentViewController(picker, animated: true, completion: nil)
+        
+        
+        }
+        
+        func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+            let chosenImage = info[UIImagePickerControllerEditedImage] as! UIImage
+            images.append(chosenImage)
+            picker.dismissViewControllerAnimated(true, completion: nil)
+            collectionView.reloadData()
+           
+            
+        }
+
+}
+
     
     //MARK: Segues
     
-}
+
