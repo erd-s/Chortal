3ddc59c6fcf8ea621aeb254f3b50d16386983b5b
Christopher Erdos <chris.erdos@gmail.com>
2016-02-16 19:41:36 -0600

    fixed rebase conflict

--- Chortal/NewTaskViewController.swift
+++ Chortal/NewTaskViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
-class NewTaskViewController: UIViewController {
+class NewTaskViewController: UIViewController, UITextFieldDelegate {
     //MARK: Properties
     
     
     //MARK: Outlets
+    
+    @IBOutlet weak var taskNameTextField: UITextField!
+    @IBOutlet weak var taskDescriptionTextField: UITextField!
+    @IBOutlet weak var incentiveTextField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var memberSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var requirePhotoSwitch: UISwitch!
+    
     
     //MARK: View Loading
     override func viewDidLoad() {
@@ -21,7 +29,18 @@ class NewTaskViewController: UIViewController {
     
     //MARK: Custom Functions
     
+    
     //MARK: IBActions
+    @IBAction func createTaskButtonTap(sender: AnyObject) {
+
+        if requirePhotoSwitch.selected {
+            //require a photo
+        }
+    }
+
+    @IBAction func clearSegmentedControlButtonTap(sender: UIButton) {
+        memberSegmentedControl.selectedSegmentIndex = UISegmentedControlNoSegment
+    }
     
     //MARK: Delegate Functions
     
