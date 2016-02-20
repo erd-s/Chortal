b55fa24105b9208f3454890726efc20ded272cb4
Christopher Erdos <chris.erdos@gmail.com>
2016-02-20 10:32:20 -0600

    added vc for taking a task and added a view, connected outlets

---
+++ Chortal/TakeTaskViewController.swift
@@ -0,0 +1,23 @@
+//
+//  TakeTaskViewController.swift
+//  Chortal
+//
+//  Created by Christopher Erdos on 2/20/16.
+//  Copyright © 2016 JonerDos. All rights reserved.
+//
+
+import Cocoa
+
+class TakeTaskViewController: UIViewController {
+
+    
+    
+    
+    
+    @IBOutlet weak var dueLabel: UILabel!
+    @IBOutlet weak var photoRequiredLabel: UILabel!
+    @IBOutlet weak var taskDescriptionLabel: UILabel!
+    @IBOutlet weak var taskNameLabel: UILabel!
+    @IBAction func takeTaskButton(sender: UIButton) {
+    }
+}
