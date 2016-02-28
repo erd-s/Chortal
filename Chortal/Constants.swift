d1dd8b1a1db9a46cedb7309f41e1140cd35c3e3e
Jon Jones <jon.p.jones13@gmail.com>
2016-02-28 12:49:19 -0600

    Added image extension for buttons

--- Chortal/Constants.swift
+++ Chortal/Constants.swift
@@ -16,6 +16,7 @@ let publicDatabase = container.publicCloudDatabase
 let orgUID = userDefaults.stringForKey("currentOrgUID")
 let memberName = userDefaults.stringForKey("currentUserName")
 let pushNotificationsSet = userDefaults.boolForKey("pushNotificationsSet")
+let chortalGreen = UIColor(red: 3, green: 117, blue: 60, alpha: 1.0)
 var currentMember: CKRecord?
 var currentOrg: CKRecord?
 var currentTask: CKRecord?
