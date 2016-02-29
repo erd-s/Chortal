4516fecda3c53109b600be21251b7064268d0191
Jon Jones <jon.p.jones13@gmail.com>
2016-02-29 12:25:52 -0600

    button changed width

--- Chortal/MemberSelectViewController.swift
+++ Chortal/MemberSelectViewController.swift
@@ -102,9 +102,6 @@ class MemberSelectViewController: UIViewController, UITableViewDataSource, UITab
         cell.layer.borderColor = UIColor.lightGrayColor().CGColor
         cell.layer.borderWidth = 0.5
         cell.contentView.frame = cell.frame
-        
-        
-        
 
         return cell
     }
