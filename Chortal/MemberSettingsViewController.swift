948b1b73798640c7e7793a5715816e3bf5c71ebd
Christopher Erdos <chris.erdos@gmail.com>
2016-02-29 14:24:22 -0600

    member settings

--- Chortal/MemberSettingsViewController.swift
+++ Chortal/MemberSettingsViewController.swift
@@ -14,11 +14,6 @@ class MemberSettingsViewController: UIViewController, UITextFieldDelegate {
     
     //MARK: Outlets
     @IBOutlet weak var multipleUsersSwitch: UISwitch!
-    @IBOutlet weak var taskApprovedSwitch: UISwitch!
-    @IBOutlet weak var taskDeniedSwitch: UISwitch!
-    @IBOutlet weak var timeRunningOutSwitch: UISwitch!
-    @IBOutlet weak var newTasksAddedSwitch: UISwitch!
-    @IBOutlet weak var taskAssignedSwitch: UISwitch!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var defaultPhotoImageView: UIImageView!
     
@@ -34,18 +29,11 @@ class MemberSettingsViewController: UIViewController, UITextFieldDelegate {
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(true)
         
-        if userDefaults.boolForKey("multipleUsers")      { multipleUsersSwitch.setOn(true, animated: true) }
-                                                    else { multipleUsersSwitch.setOn(false, animated: true) }
-        if userDefaults.boolForKey("push_taskApproved")  { taskApprovedSwitch.setOn(true, animated: true) }
-                                                    else { taskApprovedSwitch.setOn(false, animated: true) }
-        if userDefaults.boolForKey("push_taskDenied")    { taskDeniedSwitch.setOn(true, animated: true) }
-                                                    else { taskDeniedSwitch.setOn(false, animated: true) }
-        if userDefaults.boolForKey("push_timeRunningOut") { timeRunningOutSwitch.setOn(true, animated: true) }
-                                                    else { timeRunningOutSwitch.setOn(false, animated: true) }
-        if userDefaults.boolForKey("push_newTasks")      { newTasksAddedSwitch.setOn(true, animated: true) }
-                                                    else { newTasksAddedSwitch.setOn(false, animated: true) }
-        if userDefaults.boolForKey("push_taskAssigned")  { taskAssignedSwitch.setOn(true, animated: true) }
-                                                    else { taskAssignedSwitch.setOn(false, animated: true) }
+        if userDefaults.boolForKey("multipleUsers") {
+            multipleUsersSwitch.setOn(true, animated: true)
+        } else {
+            multipleUsersSwitch.setOn(false, animated: true)
+        }
         
         nameTextField.text = userDefaults.valueForKey("currentUserName") as? String
         
@@ -56,7 +44,7 @@ class MemberSettingsViewController: UIViewController, UITextFieldDelegate {
     func dismissKeyboard() {
         view.endEditing(true)
     }
-
+    
     
     //MARK: Actions
     @IBAction func changePhotoButtonTap(sender: AnyObject) {
@@ -66,17 +54,7 @@ class MemberSettingsViewController: UIViewController, UITextFieldDelegate {
     @IBAction func saveButtonTap(sender: AnyObject) {
         loadingAlert("Saving settings...", viewController: self)
         if multipleUsersSwitch.on    { userDefaults.setBool(true, forKey: "multipleUsers") }
-                                else { userDefaults.setBool(false, forKey: "multipleUsers") }
-        if taskApprovedSwitch.on     { userDefaults.setBool(true, forKey: "push_taskApproved") }
-                                else { userDefaults.setBool(false, forKey: "push_taskApproved") }
-        if taskDeniedSwitch.on       { userDefaults.setBool(true, forKey: "push_taskDenied") }
-                                else { userDefaults.setBool(false, forKey: "push_taskDenied") }
-        if timeRunningOutSwitch.on   { userDefaults.setBool(true, forKey: "push_timeRunningOut") }
-                                else { userDefaults.setBool(false, forKey: "push_timeRunningOut") }
-        if newTasksAddedSwitch.on    { userDefaults.setBool(true, forKey: "push_newTasks") }
-                                else { userDefaults.setBool(false, forKey: "push_newTasks") }
-        if taskAssignedSwitch.on     { userDefaults.setBool(true, forKey: "push_taskAssigned") }
-                                else { userDefaults.setBool(false, forKey: "push_taskAssigned") }
+        else { userDefaults.setBool(false, forKey: "multipleUsers") }
         
         if nameTextField.text != memberName {
             userDefaults.setValue(nameTextField.text, forKey: "currentUserName")
